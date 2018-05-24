5a9360e4ee20496c44a0dcb64a4425c1f0e4c94b
Khanh Nguyen <nvkhanh08t4@gmail.com>
2018-05-24 22:32:57 +0700

    Bai 16-7 Event and refs

--- my-app/src/components/Course.js
+++ my-app/src/components/Course.js
@@ -2,12 +2,48 @@ import React, { Component } from 'react';
 import Lesson from './Lesson';
 
 class Course extends Component {
+  constructor(props){
+    super(props);
+  
+    this.handleClick3 = this.handleClick3.bind(this);
+    this.registerCourse = this.registerCourse.bind(this);
+  }
+
+  handleClick1(){
+   alert("hello world");
+  }
+
+  handleClick2(content){
+    alert(content);
+   }
+
+   handleClick3(){
+    alert(this.props.name);
+   }
+
+   registerCourse(){
+     console.log(this.refs.userName.value);
+   }
+
   showButtonFree() {
     const isFree = this.props.free;
     if(isFree === true){
-      return  <div className="panel-footer">
-                <button type="button" className="btn btn-success">Register</button>
-              </div>
+      return  (
+            <div className="btn-group">
+              <button onClick={this.handleClick1} type="button" className="btn btn-warning">Register</button>
+              <button onClick={() => this.handleClick2("Hello click 2")} type="button" className="btn btn-danger">Register</button>
+              <button onClick={this.handleClick3} type="button" className="btn btn-success">Register</button>
+            </div>
+            );
+    } else {
+        return (
+              <div className="input-group">
+                <span className="input-group-btn">
+                  <button onClick={this.registerCourse} className="btn btn-info" type="button">Register</button>
+                </span>
+                <input type="text" className="form-control" placeholder="userName" ref="userName"/>
+                </div>
+              );
     }
   }
   render() {
@@ -26,7 +62,9 @@ class Course extends Component {
                   <Lesson />
                 </ul>
               </div>
-              {this.showButtonFree()}
+              <div className="panel-footer">
+                {this.showButtonFree()}
+              </div>
           </div>
         </div>
     );
